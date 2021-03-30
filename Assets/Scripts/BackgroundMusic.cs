3827faf6bc03b9a13c50a8550e5ec2e73dac2af5
Martí Gómez Prunera <magopru1995@gmail.com>
2021-03-30 11:58:35 +0200

    SFX Menu

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public enum MusicType { MENU, COMBAT }
+    public MusicType SelectedMusicType;
+    private void Start()
+    {
+        if(SelectedMusicType == MusicType.MENU)
+        {
+            AudioManager.Instance.PlaySound("Menu Music");
+            AudioManager.Instance.StopSound("Combat Music");
+        }
+        else if(SelectedMusicType == MusicType.COMBAT)
+        {
+            AudioManager.Instance.StopSound("Menu Music");
+            AudioManager.Instance.PlaySound("Combat Music");
+        }
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string Name;
+
+    public AudioClip Clip;
+
+    [Range(0.0f, 1.0f)]
+    public float Volume = 1;
+    [Range(0.3f, 3.0f)]
+    public float Pitch = 1;
+    [Range(0.0f, 1.0f)]
+    public float SpatialBlend = 0;
+
+    public bool Loop = false;
+
+    private AudioSource _source;
+
+
+    public void SetupSource(GameObject go)
+    {
+        _source = go.AddComponent<AudioSource>();
+
+        _source.clip = Clip;
+        _source.volume = Volume;
+        _source.pitch = Pitch;
+        _source.loop = Loop;
+        _source.spatialBlend = SpatialBlend;
+    }
+
+    public void Play()
+    {
+        _source.Play();
+    }
+    public void Stop()
+    {
+        _source.Stop();
+    }
+}
