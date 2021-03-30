f3d1df4b33fa80943ea23b31f0be857dc8f99325
Eric Planas Canto <noga.epc@gmail.com>
2021-03-30 17:33:30 +0200

    Student UI
    - logic

---
+++ Assets/_Project/Teacher Behaviour/Scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int Lives = 3;
+
+    private readonly List<StudentScore> _studentScore = new List<StudentScore>();
+
+    public Action<int> OnScoreUpdated;
+
+    public Action<int> OnLiveUpdated;
+
+    public int TotalScore
+    {
+        get
+        {
+            int sum = 0;
+            foreach (var studentScore in _studentScore)
+            {
+                sum += studentScore.Score;
+            }
+
+            return sum;
+        }
+    }
+
+    public void ModifyLives()
+    {
+        Lives--;
+
+        OnLiveUpdated?.Invoke(Lives);
+    }
+
+    public void UpdateScore(int score)
+    {
+        OnScoreUpdated?.Invoke(TotalScore);
+    }
+
+    public void AddNewStudentScore(StudentScore studentScore)
+    {
+        _studentScore.Add(studentScore);
+
+        studentScore.OnScoreModified += UpdateScore;
+    }
+}
---
+++ Assets/_Project/Teacher Behaviour/Scripts/Student/StudentScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class StudentScore : MonoBehaviour
+{
+    public int Score = 0;
+
+    public Action<int> OnScoreModified;
+    public void ModifyScore(int delta)
+    {
+        Score += delta;
+
+        OnScoreModified?.Invoke(Score);
+    }
+}
